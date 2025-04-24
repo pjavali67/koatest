88fad62cb750e06445374eb3de273fdd877f0eb3
pjavali67 <pjavali67@gmail.com>
2025-04-24 17:24:18 +0530

    changes config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  transpilePackages: [], // optional
+  // experimental: {
+  //   serverActions: false, // if you're on App Router and this is unused
+  // },
   /* config options here */
 };
 
